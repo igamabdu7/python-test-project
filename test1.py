5ce84303a941f9ecb9dc5299015be06c6f9dfdb3
name1 <igam.abdu7@gmail.com>
2022-11-04 21:03:43 +1000

    Initial commit

--- test1.py
+++ test1.py
@@ -1,5 +1,30 @@
-import telebot 
+import telebot.ext
+import os
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-print("Hello, World!")
+PORT = int(os.environ.get('PORT', '8443'))
 
-print("Testing")
+def start(update, context):
+    update.message.reply_text('HI!')
+
+def echo(update, context):
+    update.message.reply_text(update.message.text)
+
+def main():
+
+    TOKEN = '5706692876:AAH4koty82x0uwJtc5Pq9whVFDrM6RunQjc'
+    APP_NAME = ''
+
+    updater = Updater(TOKEN, use_context=True)
+
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    updater.start_webhook(listen="0.0.0.0", port=PORT, url_path = TOKEN, webhook_url=APP_NAME+ TOKEN)
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
