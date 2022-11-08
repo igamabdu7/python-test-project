089bd5c3b7f8c44db33a57acb145aa14a4bc9d38
name1 <igam.abdu7@gmail.com>
2022-11-08 19:43:50 +1000

    Third set

--- test1.py
+++ test1.py
@@ -1,30 +1,33 @@
-import telebot.ext
-import os
+import os 
+from datetime import datetime
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 PORT = int(os.environ.get('PORT', '8443'))
+now = datetime.now()
+strNow = now.strftime("%d/%m/%Y %H:%M:%S")
 
 def start(update, context):
-    update.message.reply_text('HI!')
+    update.message.reply_text('Hi!')
 
 def echo(update, context):
     update.message.reply_text(update.message.text)
 
+
 def main():
 
     TOKEN = '5706692876:AAH4koty82x0uwJtc5Pq9whVFDrM6RunQjc'
-    APP_NAME = 'https://telegrambeta1.herokuapp.com/'
+    APP_NAME = 'https://telegram123beta.herokuapp.com/'
 
     updater = Updater(TOKEN, use_context=True)
-
     dp = updater.dispatcher
-
-    dp.add_handler(CommandHandler("start", start))
     
+    dp.add_handler(CommandHandler("start", start))
+
     dp.add_handler(MessageHandler(Filters.text, echo))
 
     updater.start_webhook(listen="0.0.0.0", port=PORT, url_path = TOKEN, webhook_url=APP_NAME+ TOKEN)
     updater.idle()
+   
 
 if __name__ == '__main__':
     main()
